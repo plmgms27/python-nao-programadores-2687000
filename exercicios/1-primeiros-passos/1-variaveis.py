f2ccf8273e977abbef086f3f421d6604dc2e1978
Paloma Gomes <71579559+plmgms27@users.noreply.github.com>
2024-11-03 01:24:40 +0000

    resolução exercício variáveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,15 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'paloma'
+print (nome)
 
 # Declare variáveis de cada tipo numérico
-
+numero = 27
+mes = 10
+print (numero, mes)
 
 # Declare uma variável do tipo booleano
+falso = False
+verdadeiro = True
+print (falso)
