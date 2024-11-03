5beb4e9af0c48a61e8c0f48661e1c2f262660e36
Paloma Gomes <71579559+plmgms27@users.noreply.github.com>
2024-11-03 01:54:53 +0000

    resolução exercícios numéricos

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,14 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
-
+frase = "Paloma nasceu em "
+print(frase + data_nascimento)
 # Realize uma operação entre dados do tipo inteiro e float
+soma = idade_numerica + altura
+print(soma)
+round(soma,1)
